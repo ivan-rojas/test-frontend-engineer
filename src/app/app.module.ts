24708410e12ae78a1770d05ea1127f89281528c8
Lopez <ivan.rojas@outlook.com>
2021-01-21 12:22:12 -0300

    🚀 - Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { PinchZoomModule } from 'ngx-pinch-zoom';
+
+// Components
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    PinchZoomModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
